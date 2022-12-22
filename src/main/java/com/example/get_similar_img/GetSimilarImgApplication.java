4a8491584fa165c6f33e90b9d37b9c5cac36df06
안승현 <>
2022-12-22 18:22:28 +0900

    first commit

---
+++ src/main/java/com/example/get_similar_img/GetSimilarImgApplication.java
@@ -0,0 +1,13 @@
+package com.example.get_similar_img;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GetSimilarImgApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GetSimilarImgApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/example/get_similar_img/GetSimilarImgApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.get_similar_img;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GetSimilarImgApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
